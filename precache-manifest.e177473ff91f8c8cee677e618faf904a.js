203e7ffea8e1430df74d1be86cf95f44a276fc3f
Baskaranxiphias <baskaran@xiphiastec.com>
2019-03-21 01:16:38 +0530

    Updates

---
+++ precache-manifest.e177473ff91f8c8cee677e618faf904a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dbaskaranMCA/poforaeries/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f954f1e4becddf72489d",
+    "url": "/dbaskaranMCA/poforaeries/static/js/runtime~main.c9359e4d.js"
+  },
+  {
+    "revision": "5963f7fd35bdb800e234",
+    "url": "/dbaskaranMCA/poforaeries/static/js/main.161ead56.chunk.js"
+  },
+  {
+    "revision": "436ac4b64f8ed63b0006",
+    "url": "/dbaskaranMCA/poforaeries/static/js/2.7108bfe2.chunk.js"
+  },
+  {
+    "revision": "5963f7fd35bdb800e234",
+    "url": "/dbaskaranMCA/poforaeries/static/css/main.ff4ba2d9.chunk.css"
+  },
+  {
+    "revision": "2f5a7158b161a1c25c6fa0c0090e696f",
+    "url": "/dbaskaranMCA/poforaeries/index.html"
+  }
+];
